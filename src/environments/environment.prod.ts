9cd30f92a77702a245d5049b006d0e04787b487b
duvu <hoaivubk@gmail.com>
2020-10-28 22:29:46 +0700

    youtube plus: angular + firebase + ngrx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA2LuD9GbUC9vjxdN7BdyFluRjnhZmUdGk",
-    authDomain: "sprodomxxx.firebaseapp.com",
-    databaseURL: "https://sprodomxxx.firebaseio.com",
-    projectId: "sprodomxxx",
-    storageBucket: "sprodomxxx.appspot.com",
-    messagingSenderId: "738018784500",
-    appId: "1:738018784500:web:96812224dda398a5"
+    // add firebase config here.
   }
-
-
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyA2LuD9GbUC9vjxdN7BdyFluRjnhZmUdGk",
-    authDomain: "sprodomxxx.firebaseapp.com",
-    databaseURL: "https://sprodomxxx.firebaseio.com",
-    projectId: "sprodomxxx",
-    storageBucket: "sprodomxxx.appspot.com",
-    messagingSenderId: "738018784500",
-    appId: "1:738018784500:web:96812224dda398a5"
+    // add firebase config here.
   }
 };
 
